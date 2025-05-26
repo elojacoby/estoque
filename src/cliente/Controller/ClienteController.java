14742b69f8afdb4e56d2dda821e5cbb707b730e2
Luana Schorr <luanaschorr02@gmail.com>
2025-05-26 19:37:54 -0300

    Subindo arquivos faltantes

--- src/cliente/Controller/ClienteController.java
+++ src/cliente/Controller/ClienteController.java
@@ -14,28 +14,5 @@ public class ClienteController{
     }
     public void setBuscarScreen(BuscarScreen buscarScreen) {
         this.buscarScreen = buscarScreen;
-        configurarListenersBusca();
-    }
-    private void configurarListenersBusca() {
-        buscarScreen.setBuscarListener(e -> buscarProduto());
-        // buscarScreen.setSalvarListener(e -> salvarProduto());
-        buscarScreen.setLimparListener(e -> buscarScreen.limparCampos());
-        // buscarScreen.setVoltarListener(e -> voltarParaInicial());
-    }
-    private void buscarProduto() {
-        String nome = buscarScreen.getNomeProduto();
-        // Implemente a busca na API aqui
-        // produtoAtual = apiService.buscarProduto(nome);
-        
-        if (produtoAtual != null) {
-            buscarScreen.mostrarResultadoBusca(
-                produtoAtual.getNome(),
-                produtoAtual.getDescricao(),
-                produtoAtual.getPreco(),
-                produtoAtual.getQuantidade()
-            );
-        } else {
-            buscarScreen.mostrarMensagem("Produto não encontrado!");
-        }
     }
 }
